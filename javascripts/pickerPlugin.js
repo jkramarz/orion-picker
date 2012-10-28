8f092d0a3cbee56fbad181284ef078273df461bd
Jakub Kramarz <lenwe@lenwe.net>
2012-10-28 09:03:39 -0400

    img src guessing

--- javascripts/pickerPlugin.js
+++ javascripts/pickerPlugin.js
@@ -6,12 +6,27 @@
  
 function fun(){
 	if (w.closed){
-		console.log('fun call pre');
+		//check youtube or image
+		if(url.indexOf("http://www.youtube.com/watch?v") == -1) {
+			//check public-domain-image
+			if(url.indexOf("http://www.public-domain-image.com") != -1) {
+				var u = url.replace("full-image", "public-domain-images-pictures-free-stock-photos");
+				var t =u.indexOf(".jpg");
+				url = u.slice(0, t+4);
+			}
+			//check flickr
+			if(url.indexOf("http://www.fotopedia.com/") != -1) {
+				var u = url.replace("http://www.fotopedia.com/items/flickr-", "http://images.cdn.fotopedia.com/flickr-");
+				url = u.concat("-hd.jpg");
+			
+			}
+		
+		}
 		deferred.resolve(url);
-		console.log('fun call post, result:' + url);
+		//console.log('fun call post, result:' + url);
 		return;
 	}
-	console.log('fun fun');
+	//console.log('fun fun');
 	setTimeout(function(){
 		fun();
 	}, 1000);  
@@ -19,18 +34,12 @@ function fun(){
  
  
 function createPicker(text) {
-
 	deferred = new orion.Deferred();
-
 	url = '';
 	picked = false;
-    w = window.open('picker.html','color_popup','width=610,height=550,scrollbars=0');
-    
+    w = window.open('picker.html','color_popup','width=800,height=600,scrollbars=0');
  	w.focus();
- 	
  	fun();
- 	
- 	
    return deferred;
 }
 
@@ -53,7 +62,7 @@ window.onload = function() {
 		}
 	};
 	var serviceProps = {
-		name: "Pick an image",
+		name: "Google Picker",
 		key: ["p", true, true]
 	};
 	provider.registerServiceProvider("orion.edit.command", serviceImpl, serviceProps);
